fa96f0616cc6e7f1f0fd61c6916287fea6f71114
anantayarikhi <anantayarikhi@gmail.com>
2026-08-23 21:24:41 +0530

    DAY5

---
+++ day5ques2.c
@@ -0,0 +1,12 @@
+//Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+#include<stdio.h>
+int main(){
+    int seconds, hours, minutes;
+    printf("Enter time in seconds:");
+    scanf("%d", &seconds);
+    hours=seconds/3600;
+    minutes=(seconds%3600)/60;
+    seconds=seconds%60;
+    printf("time in hours:minutes:seconds format is %d:%d:%d\n", hours, minutes, seconds);
+    return 0;
+}
