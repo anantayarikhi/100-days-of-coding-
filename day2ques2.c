6696c7a8cd99231f1198291d1a083a2dfba48cfb
anantayarikhi <anantayarikhi@gmail.com>
2026-08-23 15:25:43 +0530

    DAY2

---
+++ day2ques2.c
@@ -0,0 +1,13 @@
+//Q4: Write a program to calculate the area and circumference of a circle given its radius.
+#include<stdio.h>
+int main() {
+    float radius, area, circumference;
+    const float PI =3.14;
+    printf("Enter the radius of circle:");
+    scanf("%f", &radius);
+    area = PI * radius*radius;
+    circumference = 2 * PI * radius;
+    printf("Area of circle: %.2f\n", area);
+    printf("Circumference of circle: %.2f\n", circumference);
+    return 0;
+}
