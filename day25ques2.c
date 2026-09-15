30f0c4aa94c7ddcad8c6ec67970a7de1e626b9aa
anantayarikhi <anantayarikhi@gmail.com>
2026-09-15 18:43:47 +0530

    DAY 25

---
+++ day25ques2.c
@@ -0,0 +1,24 @@
+//Q50: Write a program to print the following pattern:
+//*****
+//****
+//***
+//**
+//*
+
+#include <stdio.h>
+int main()
+{
+    for(int i = 5; i >= 1; i--)
+    {
+        for(int j = 5; j > i; j--)
+        {
+            printf(" ");
+        }
+        for(int k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
