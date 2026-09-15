617a97f5320b96fc8868b79560817b665ffebe32
anantayarikhi <anantayarikhi@gmail.com>
2026-09-15 18:22:06 +0530

    DAY 18

---
+++ day18ques2.c
@@ -0,0 +1,20 @@
+//Q36: Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    while(b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("%d", a);
+
+    return 0;
+}
